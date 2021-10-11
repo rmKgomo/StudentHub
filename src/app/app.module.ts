7875cfbef65c51cf8886179e75eca142dd09df79
Rofhiwa Kgomo <kgomo@thedigitalacademy.co.za>
2021-10-11 16:43:17 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
@@ -23,6 +27,7 @@ import { AppRoutingModule } from './app-routing.module';
     MDBBootstrapModule.forRoot(),
     FormsModule,
     AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+
+  successMessage:string ="";
+
+  loginForm!: FormGroup; 
+
+
+  constructor(private fb: FormBuilder, public router : Router) { }
+
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email:['',[Validators.required, Validators.pattern("[A-Za-z0-9]*@gmail.com")]],
+      password:['',[Validators.required,Validators.pattern("[A-Za-z0-9@!_]{6,}")]]
+    })
+
+  }
+
+  registerRoute() {
+    this.router.navigate(['/register'])
+  }
+
+
+
+  login(){
+    this.successMessage="Successfully Loggined In..."
+  }
+
+}
