76ba9deb293239bde4fe24ea88cd1c98ac29eae5
Rofhiwa Kgomo <kgomo@thedigitalacademy.co.za>
2021-10-15 16:28:10 +0200

    profiledone

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcademicReportsComponent } from './components/academic-reports/academic-reports.component';
 import { CovidComponent } from './components/covid/covid.component';
 import { HomeComponent } from './components/home/home.component';
+import { InboxComponent } from './components/inbox/inbox.component';
 import { LoginComponent } from './components/login/login.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
 
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, children: [
+    {path: '', component: CovidComponent, pathMatch: 'full'},
+    {path: 'profile', component: ProfileComponent, pathMatch: 'full'},
+    {path: 'inbox', component: InboxComponent, pathMatch: 'full'},
+    {path: 'academic', component: AcademicReportsComponent, pathMatch: 'full'},
+    {path: 'settings', component: SettingsComponent, pathMatch: 'full'},
+    { path: '**', redirectTo: '', pathMatch: 'full' },
+  ] },
   {path: '', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
   { path: '**', redirectTo: 'login', pathMatch: 'full' },
--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -19,7 +19,6 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {
     this.regForm = this.fb.group({
       name: ['',[Validators.required]],
-      mobileNumber: ['',[Validators.required, Validators.min(1000000000),Validators.max(9999999999)]],
       email:['',[Validators.required, Validators.pattern("[a-zA-Z0-9]*@gmail.com")]],
       password: ['',[Validators.required,Validators.pattern("[a-zA-z@_]{6,}")]]
     })
